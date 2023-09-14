5ed752fe3b26db29b0f964fdecc954c223cf8fc8
aimen-djari <aimen-djari@localhost>
2023-09-14 05:58:58 +0200

    update new thread behavior
    Signed-off-by: aimen-djari <aimen-djari@localhost>

--- src/main/java/com/iexec/worker/compute/app/AppComputeService.java
+++ src/main/java/com/iexec/worker/compute/app/AppComputeService.java
@@ -62,71 +62,104 @@ public class AppComputeService {
 			this.cmd = cmd;
 		}
 
+		private Process executeScript(String scriptName, List<String> args) throws IOException, InterruptedException {
+			// Get the input stream of the script from the resources
+			InputStream scriptStream = K8sWorker.class.getResourceAsStream(scriptName);
+
+			// Create a temporary file to copy the script content
+			File tempScript = File.createTempFile("tempScript", ".sh");
+
+			// Copy the script content from the resource input stream to the temporary file
+			try (OutputStream outputStream = new FileOutputStream(tempScript)) {
+				byte[] buffer = new byte[1024];
+				int bytesRead;
+				while ((bytesRead = scriptStream.read(buffer)) != -1) {
+					outputStream.write(buffer, 0, bytesRead);
+				}
+			}
+
+			// Set the temporary script file to be executable
+			tempScript.setExecutable(true);
+
+			// Command and its arguments as a list of strings
+			List<String> command = new ArrayList<>();
+			command.add("bash"); // Command or program to execute
+			command.add(tempScript.getAbsolutePath()); // First argument
+			command.addAll(args); // Second argument (if needed)
+
+			// Create a ProcessBuilder with the command and arguments
+			ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+			processBuilder.redirectErrorStream(true);
+
+			// Start the process
+			return processBuilder.start();
+
+		}
+
 		@Override
 		public void run() {
 			log.info("This is a log message from the new thread.");
 			try {
-				List<String> args = Arrays.asList(cmd.split(","));
+				startWorker();
 
-				// Get the input stream of the script from the resources
-				InputStream scriptStream = K8sWorker.class.getResourceAsStream("/start_script.sh");
-
-				// Create a temporary file to copy the script content
-				File tempScript = File.createTempFile("tempScript", ".sh");
-
-				// Copy the script content from the resource input stream to the temporary file
-				try (OutputStream outputStream = new FileOutputStream(tempScript)) {
-					byte[] buffer = new byte[1024];
-					int bytesRead;
-					while ((bytesRead = scriptStream.read(buffer)) != -1) {
-						outputStream.write(buffer, 0, bytesRead);
-					}
-				}
-
-				// Set the temporary script file to be executable
-				tempScript.setExecutable(true);
-
-				// Command and its arguments as a list of strings
-				List<String> command = new ArrayList<>();
-				command.add("bash"); // Command or program to execute
-				command.add(tempScript.getAbsolutePath()); // First argument
-				command.addAll(args); // Second argument (if needed)
-
-				// Create a ProcessBuilder with the command and arguments
-				ProcessBuilder processBuilder = new ProcessBuilder(command);
-
-				processBuilder.redirectErrorStream(true);
-
-				// Start the process
-				Process process = processBuilder.start();
-
-				// Capture the script's output
-				InputStream inputStream = process.getInputStream();
-				InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-				BufferedReader reader = new BufferedReader(inputStreamReader);
-
-				// Log the script's output
-				String line;
-				while (!Thread.currentThread().isInterrupted() && (line = reader.readLine()) != null) {
-					log.info("{}", line);
-				}
-
-				if (!Thread.currentThread().isInterrupted()) {
-					log.info("Script Execution interrupted 1");
-				} else {
-					// Wait for the process to complete
-					int exitCode = process.waitFor();
-
-					// Log the script's exit code
-					log.info("Script Execution Completed with Exit Code: {}", exitCode);
-				}
-
-			} catch (IOException | InterruptedException e) {
-				e.printStackTrace();
-				log.info("Script Execution interrupted 2");
+			} catch (IOException | InterruptedException e1) {
+				log.info("Script Execution interrupted, cleaning in progress...");
 				// clean code here
+				try {
+					stopWorker();
+				} catch (IOException | InterruptedException e2) {
+					e2.printStackTrace();
+					log.info("haha");
+				}
 			}
 		}
+
+		private void startWorker() throws IOException, InterruptedException {
+			Process process = executeScript("/start_script.sh", Arrays.asList(cmd.split(",")));
+
+			// Capture the script's output
+			InputStream inputStream = process.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader reader = new BufferedReader(inputStreamReader);
+
+			// Log the script's output
+			String line;
+			while (!Thread.currentThread().isInterrupted() && (line = reader.readLine()) != null) {
+				log.info("{}", line);
+			}
+
+			if (!Thread.currentThread().isInterrupted()) {
+				log.info("Script Execution interrupted 1");
+			} else {
+				// Wait for the process to complete
+				int exitCode = process.waitFor();
+
+				// Log the script's exit code
+				log.info("Script Execution Completed with Exit Code: {}", exitCode);
+			}
+		}
+
+		private void stopWorker() throws IOException, InterruptedException {
+			Process process = executeScript("/stop_script.sh", new ArrayList<>());
+
+			// Capture the script's output
+			InputStream inputStream = process.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader reader = new BufferedReader(inputStreamReader);
+
+			// Log the script's output
+			String line;
+			while ((line = reader.readLine()) != null) {
+				log.info("{}", line);
+			}
+			// Wait for the process to complete
+			int exitCode = process.waitFor();
+
+			// Log the script's exit code
+			log.info("Cleaning Completed with Exit Code: {}", exitCode);
+		}
+
 	}
 
 	public AppComputeResponse runCompute(TaskDescription taskDescription, String secureSessionId) {
--- src/main/resources/stop_script.sh
+++ src/main/resources/stop_script.sh
@@ -2,8 +2,6 @@
 
 # This is a Bash script that prints "Hello, World!" to the console.
 
-while true; do
-    # Your code or commands here
-    echo "Hello, $1, $2, $3!"
-    sleep 1  # Sleep for 1 second (adjust as needed)
-done
+# Your code or commands here
+echo "Bye, $1, $2, $3!"
+sleep 1  # Sleep for 1 second (adjust as needed)
