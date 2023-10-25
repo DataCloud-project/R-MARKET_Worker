8d5e35859a1706ad7d856bdcc36fc8a4836f8df7
aimen-djari <aimen-djari@mbp-de-aimen>
2023-10-25 11:57:45 +0200

    update script
    Signed-off-by: aimen-djari <aimen-djari@mbp-de-aimen>

--- deployment/docker/script.sh
+++ deployment/docker/script.sh
@@ -2,6 +2,9 @@
 
 sudo chmod 666 /var/run/docker.sock
 iexec wallet create --keystoredir . --password setim
+sudo rm wallet_worker1.json
+sudo mv UTC*.json wallet_worker1.json
 docker network create iexec-worker-net
 sudo apt-get install docker-ce=5:20.10.21~3-0~ubuntu-focal
 sudo apt-get install docker-ce-cli=5:20.10.21~3-0~ubuntu-focal
+
